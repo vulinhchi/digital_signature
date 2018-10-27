e49d729f9249a4988f4b9436e2ab8008a8eb919e
vulinhchi <vulinhchi.05@gmail.com>
2018-10-27 22:15:26 +0700

    setup route

---
+++ bc_signature/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+import bc_signature.views as bc_signature_views
+
+app_name = 'bc_signature'
+urlpatterns = [
+    path('', bc_signature_views.Home.as_view(), name = 'home')
+]
