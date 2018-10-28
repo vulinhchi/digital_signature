5c6a57c889035f1b5096275c1f3a7fdb204685dd
vulinhchi <vulinhchi.05@gmail.com>
2018-10-28 16:51:13 +0700

    template for signup, login

--- digital_sign/settings.py
+++ digital_sign/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -114,6 +114,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+
+]
+
+MEDIA_URL = '/static/media/'
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'static/media/')
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 
 LOGIN_URL = LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL ='/'
