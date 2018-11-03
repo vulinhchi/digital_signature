7c85f5c8588e5cd373e41ba36b0af812e5abee64
vulinhchi <vulinhchi.05@gmail.com>
2018-11-03 10:56:02 +0700

    sign/verify and save new transaction on BC

--- bc_signature/views.py
+++ bc_signature/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib import messages
 from django.views import generic
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
@@ -38,13 +39,9 @@ def RegisterWallet(request):
     list_user_id_in_wallets = []
     if request.user.is_authenticated:
         id_user = request.user.id
-        wallets = models.WalletAccount.objects.all() # list all wallet
-        u = models.User.objects.get(id=id_user) # user hien tai.
-       
-        for w in wallets:
-            list_user_id_in_wallets.append(w.user.id)
-        
-        if u.id not in list_user_id_in_wallets: # check if user had not created wallet account yet
+        u = models.User.objects.get(id=id_user)
+        result , list_user_id_in_wallets = check_wallet_account_exist(id_user)
+        if not result:
             wallet = models.WalletAccount() 
             rep = requests.post(f'http://172.30.0.1:2202/account/{id_user}')
             data = rep.json()
@@ -54,10 +51,10 @@ def RegisterWallet(request):
             wallet.wallet_account  = data['address']
             wallet.save()
         else:
-            data = 'user already had an wallet account'
+            data = 'user already have an wallet account'
             info = ""
             for item in list_user_id_in_wallets:
-                if u.id == item:
+                if id_user == item:
                     i = models.WalletAccount.objects.get(user=u)
                     info = f"account : {i.wallet_account} and private_key: {i.wallet_private_key}"
             data += info
@@ -71,52 +68,113 @@ def ResgisterRSA(request):
     list_user_id_has_rsa_key = []
     if request.user.is_authenticated:
         id_user = request.user.id
-        rsa_keys = models.RSAAccount.objects.all()
         u = models.User.objects.get(id=id_user)
-        mess = "ahihi"
-        
-        print(" kieu: ", RSA.generate(1024).exportKey())
-        a = RSA.generate(1024)
-        print(" a = ",type(a)) # <class 'Crypto.PublicKey.RSA._RSAobj'>
-        for key in rsa_keys:
-            list_user_id_has_rsa_key.append(key.user.id)
-        if u.id not in list_user_id_has_rsa_key: # check if user had not created wallet account yet
+        result, list_user_id_has_rsa_key = check_RSA_account_exist(id_user)
+        if  not result:
             rsa_key = models.RSAAccount()
             prikey = RSA.generate(1024)
-            print('type pri', prikey) # <_RSAobj @0x7fdee9260d68 n(1024),e,d,p,q,u,private>
             pubkey = prikey.publickey()
-            print(type(prikey)) #<class 'Crypto.PublicKey.RSA._RSAobj'>
             # save json in models:
             rsa_key.user = u
-            rsa_key.rsa_private_key = prikey.exportKey()
-            rsa_key.rsa_public_key  = pubkey.exportKey()
+            rsa_key.rsa_private_key = prikey.exportKey().decode() # save the key in DB by String
+            rsa_key.rsa_public_key  = pubkey.exportKey().decode()
             print('sau khi luu vao DB',type(rsa_key.rsa_private_key)) # <class 'bytes'>
             rsa_key.save()
             print('sau khi luu vao DB 2 ',type(rsa_key.rsa_private_key)) # <class 'bytes'>
             data = f'publickey = {pubkey} and private key = {prikey}'
         else:
-            data = 'user already had an rsa keypair  '
+            data = 'user already have an rsa keypair  '
             info = ""
             for item in list_user_id_has_rsa_key:
-                if u.id == item:
+                if id_user == item:
                     i = models.RSAAccount.objects.get(user=u)
-                    print(type(i.rsa_private_key)) #<class 'str'>
                     pub = i.rsa_public_key
                     pri = i.rsa_private_key
+                    
                     info = f"publickey : {pub} and private_key: {pri}"
             data += info
     else:
         data = "User need to log in!"           
     return render(request, 'get_account.html', {'data':data})
 
-    
+
+def check_RSA_account_exist(user_id):
+    list_user_id_has_rsa_key = []
+    u = models.User.objects.get(id=user_id)
+    rsa_keys = models.RSAAccount.objects.all()
+    for key in rsa_keys:
+            list_user_id_has_rsa_key.append(key.user.id)
+    if u.id not in list_user_id_has_rsa_key:
+        return False, list_user_id_has_rsa_key
+    else:
+        return True, list_user_id_has_rsa_key
+
+
+def check_wallet_account_exist(user_id):
+    list_user_id_has_wallet_key = []
+    u = models.User.objects.get(id=user_id)
+    wallet_keys = models.WalletAccount.objects.all()
+    for key in wallet_keys:
+            list_user_id_has_wallet_key.append(key.user.id)
+    if u.id not in list_user_id_has_wallet_key:
+        return False, list_user_id_has_wallet_key
+    else:
+        return True, list_user_id_has_wallet_key
+
+
 def sign_contract(request):
     template_sign = loader.get_template('base.html')
     try:
         content = request.POST['content']
         print(content)
-        return HttpResponse(template_sign.render({
-			'message': content
-		}, request))
+        if len(content) == 0:
+            messages.error(request, 'You need to type something!')
+        # check have user has RSA account and wallet account?
+        if request.user.is_authenticated:
+            id_user = request.user.id
+            u = models.User.objects.get(id=id_user)
+            result_rsa, list_user_id_has_rsa_key = check_RSA_account_exist(id_user)
+            result_wallet , list_user_id_in_wallets = check_wallet_account_exist(id_user)
+            if result_rsa and result_wallet:
+                # sign
+                for item in list_user_id_has_rsa_key:
+                    if id_user == item:
+                        i = models.RSAAccount.objects.get(user=u)
+                        
+                        pub = i.rsa_public_key
+                        pri = i.rsa_private_key
+                        
+                        pub_import = RSA.importKey(pub)
+                        pri_import = RSA.importKey(pri)
+                        
+                        signature = pri_import.sign(content.encode(),10)
+                        
+                        # verify:
+                        result = pub_import.verify(content.encode(),signature)
+                        print("ket qua: ", result)
+                        # save on blokchhain: 
+                        if result:
+                            transaction = {
+                                'from': "43430909990",
+                                'to':'34jer84838',
+                                'data':
+                                {
+                                    'text': content,
+                                    'signature': signature,
+                                    'public_key': pub,
+                                    'user_id': id_user,
+                                    'username': u.username
+                                }
+                            }
+                            rep = requests.post('http://172.30.0.1:2201/transactions', headers=headers, data= json.dumps(transaction))
+                            data = rep.json()
+            
+            elif not result_rsa:
+                messages.error(request, "You should need a RSA account")
+            elif not result_wallet:
+                messages.error(request, "You should need a wallet account ")
+        else:
+            messages.error(request, 'You have to login!')
+        return render(request, 'base.html')
     except:
-        return HttpResponse(template_sign.render({}, request))
+        return render(request, 'base.html')
